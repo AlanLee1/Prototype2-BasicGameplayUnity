4d2d264f2dc18acda216bebbc67460453f8593c6
alan.lee <alan.lee@pti.org.br>
2022-05-06 16:14:14 -0300

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    [Range(10,30)]
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    public GameObject projectilePrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AtivateProjectile();
+        LimitAreaPlayer();
+        MovePlayer();
+
+    }
+
+    private void AtivateProjectile()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //instanciar uma cópia do objeto na tela
+            //Objeto, Posição (referenciando a posicao do player), Rotação
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+
+    private void LimitAreaPlayer()
+    {
+        //Condição para limitar a posição do objeto
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
